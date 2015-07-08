3046462b62211093aa62f9a16bbc0923577c5a62
babangithub <babangithub@excite.co.jp>
2015-07-08 02:38:01 +0000

    cap-test3

--- cap-pro/config/deploy/manage.rb
+++ cap-pro/config/deploy/manage.rb
@@ -1,10 +1,10 @@
-# サーバ情報を記載した./config/servers.rbをロードするように変更
 load './config/servers.rb'
  
 task :mkdir do
   dir = '/root/test'
  
-  on roles( :client ) do
+  # 同時実行数を2サーバとし、インターバルを3秒とする
+  on roles( :client ), in: :groups, limit: 2, wait: 3 do
     execute "mkdir -p #{dir}"
     execute "ls -ld #{dir}"
   end
