85f342af1b99e6f806b55d7abd1c2c95778a3d11
babangithub <babangithub@excite.co.jp>
2015-07-08 02:26:24 +0000

    cap-test3

--- cap-pro/config/deploy/manage.rb
+++ cap-pro/config/deploy/manage.rb
@@ -1,26 +1,12 @@
-# userがroot,パスワードが****にてログインさせる
-SSHKit::Backend::Netssh.configure do |ssh|
-    ssh.ssh_options = {
-    :user =>    'root',
-    :password =>    'passroot',
-}
-end
+# サーバ情報を記載した./config/servers.rbをロードするように変更
+load './config/servers.rb'
  
-# chefというホスト名のサーバのロール名をclientとする
-server 'chef', :roles =>    [ :client ]
- 
-# タスク名をmkdirにする
 task :mkdir do
+  dir = '/root/test'
  
-    # dir変数を設定する
-    dir = '/root/test'
- 
-    # ロール名がclientに対し,do～endまでに記載したコマンドを実行する
-    on roles( :client ) do
- 
-        # mkdirとlsコマンドを実行する
-        execute "mkdir -p #{dir}"
-        execute "ls -ld #{dir}"
-    end
+  on roles( :client ) do
+    execute "mkdir -p #{dir}"
+    execute "ls -ld #{dir}"
+  end
 end
  
